b0523363dbc11c562a8cd418570c046659bf72d2
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-10-20 12:24:02 +0100

    Added tenantid and created storage container

--- modules/azure/storage/main.tf
+++ modules/azure/storage/main.tf
@@ -1,6 +1,16 @@
+provider "azurerm" {
+  tenant_id = "7049e6a3-141d-463a-836b-1ba40d3ff653"
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_resource_group" {
+  name     = "tfstateresourcegroup"
+  location = var.location
+}
+
 resource "azurerm_storage_account" "tf_storage" {
-  name                     = "tfstatestorage"
-  resource_group_name      = var.resource_group_name
+  name                     = "tfstatestorageaccountgw"
+  resource_group_name      = azurerm_resource_group.tf_resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
@@ -8,6 +18,6 @@ resource "azurerm_storage_account" "tf_storage" {
 
 resource "azurerm_storage_container" "tf_container" {
   name                  = "tfstatecontainer"
-  storage_account_name  = azurerm_storage_account.etf_storage.name
+  storage_account_name  = azurerm_storage_account.tf_storage.name
   container_access_type = "private"
 }
--- modules/azure/storage/variables.tf
+++ modules/azure/storage/variables.tf
@@ -1,19 +1,17 @@
-variable "resource_group_name" {
-  description = "The resource group name"
-  type        = string
-}
-
 variable "location" {
   description = "The Azure Location"
   type        = string
+  default     = "UK South"
 }
 
 variable "account_tier" {
   description = "The storage account tier"
   type        = string
+  default     = "Standard"
 }
 
 variable "account_replication_type" {
   description = "The storage account replication type"
   type        = string
+  default     = "LRS"
 }
