136615a8d99a469eecf4b80ed56beed5d440b634
Sylvester Loreto <sylvester.loreto@codurance.com>
2020-10-20 17:53:44 +0100

    Adding key vault to terraform

---
+++ modules/azure/key-vault/variables.tf
@@ -0,0 +1,11 @@
+variable "azure_region" {
+  description = "Metadata Azure Region"
+  type        = string
+  default     = "uksouth"
+}
+
+variable "resource_group" {
+  description = "Azure Resource Group"
+  type        = string
+  default     = "keyvault"
+}
