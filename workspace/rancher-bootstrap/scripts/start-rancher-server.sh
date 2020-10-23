21b4439a6c14c6c17ae8ed168598a024add228b2
Tass Skoudros <tass@servana.net>
2020-10-23 10:12:41 +0100

    turn off firewall for now

--- workspace/rancher-bootstrap/scripts/start-rancher-server.sh
+++ workspace/rancher-bootstrap/scripts/start-rancher-server.sh
@@ -5,12 +5,13 @@ yum -y install docker
 mkdir -p /opt/rancher-data/mysql
 mkdir -p /opt/rancher-data/rancher
 
+systemctl disable firewalld
+
 service docker start
 
 docker run -d --restart=unless-stopped \
 -v /opt/rancher-data/mysql:/var/lib/mysql \
 -v /opt/rancher-data/rancher:/var/lib/rancher \
 -p 80:80 -p 443:443 \
---network host \
 --privileged \
 rancher/rancher:latest
