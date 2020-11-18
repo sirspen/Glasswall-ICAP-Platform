faedc1a2dee37f5b18b66873787a4da719ea5f79
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-18 17:06:46 +0000

    Add icap adaptation app

--- modules/rancher/catalog_apps/main.tf
+++ modules/rancher/catalog_apps/main.tf
@@ -1,6 +1,23 @@
-resource "rancher2_catalog_v2" "foo" {
+resource "rancher2_catalog_v2" "catalogue" {
   cluster_id = var.cluster_id
-  name       = "foo"
+  name       = "catalogue"
   git_repo   = var.helm_charts_repo_url
   git_branch = var.helm_charts_repo_branch
 }
+
+resource "rancher2_project" "icapservice" {
+  name = "icapservice"
+  cluster_id = var.cluster_id
+}
+
+resource "rancher2_multi_cluster_app" "icapadaptation" {
+  catalog_name = rancher2_catalog_v2.catalogue.name
+  name = "icapadaptation"
+  targets {
+    project_id = rancher2_project.icapservice.id
+  }
+  template_name = "icap-adaptation"
+  template_version = "0.0.3"
+  roles = ["project-member"]
+}
+
