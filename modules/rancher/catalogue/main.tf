361fef77a21b7e87bd057c9b1fe2da6cb81235a2
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-19 17:02:47 +0000

    Rename rancherv2 catalogue

--- modules/rancher/catalogue/main.tf
+++ modules/rancher/catalogue/main.tf
@@ -6,8 +6,14 @@ provider "rancher2" {
 }
 
 resource "rancher2_catalog" "catalogue" {
-  cluster_id = var.cluster_id
   name       = "catalogue"
   url        = var.helm_charts_repo_url
   branch     = var.helm_charts_repo_branch
 }
+
+//resource "rancher2_catalog_v2" "cataloguev2" {
+//  cluster_id  = var.cluster_id
+//  name        = "cataloguev2"
+//  git_repo    = var.helm_charts_repo_url
+//  git_branch  = var.helm_charts_repo_branch
+//}
