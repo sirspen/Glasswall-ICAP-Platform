ef25d290c0798df6ca6f7926ecee64ebf3dbb16b
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-19 13:26:49 +0000

    Add rancherv2 catalogue

--- modules/rancher/catalog_apps/main.tf
+++ modules/rancher/catalog_apps/main.tf
@@ -1,42 +1,13 @@
-resource "rancher2_catalog" "catalogue" {
+//resource "rancher2_catalog" "catalogue" {
+//  cluster_id = var.cluster_id
+//  name       = "catalogue"
+//  url        = var.helm_charts_repo_url
+//  branch     = var.helm_charts_repo_branch
+//}
+
+resource "rancher2_catalog_v2" "cataloguev2" {
   cluster_id = var.cluster_id
-  name       = "catalogue"
-  url        = var.helm_charts_repo_url
-  branch     = var.helm_charts_repo_branch
+  name       = "cataloguev2"
+  git_repo = var.helm_charts_repo_url
+  git_branch = var.helm_charts_repo_branch
 }
-
-resource "rancher2_project" "icapservice" {
-  name       = "icapservice"
-  cluster_id = var.cluster_id
-}
-
-resource "rancher2_multi_cluster_app" "icapadaptation" {
-  catalog_name = rancher2_catalog.catalogue.name
-  name         = "icapadaptation"
-  targets {
-    project_id = rancher2_project.icapservice.id
-  }
-  template_name = "icap-adaptation"
-  roles         = ["cluster-owner"]
-}
-
-resource "rancher2_multi_cluster_app" "rabbitmq" {
-  catalog_name = rancher2_catalog.catalogue.name
-  name         = "rabbitmq"
-  targets {
-    project_id = rancher2_project.icapservice.id
-  }
-  template_name = "icap-rabbitmq"
-  roles         = ["cluster-owner"]
-}
-
-resource "rancher2_multi_cluster_app" "systemclusterrole" {
-  catalog_name = rancher2_catalog.catalogue.name
-  name         = "systemclusterrole"
-  targets {
-    project_id = rancher2_project.icapservice.id
-  }
-  template_name = "systemclusterrole"
-  roles         = ["cluster-owner"]
-}
-
