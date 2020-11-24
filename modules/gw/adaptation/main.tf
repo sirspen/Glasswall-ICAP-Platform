454e9010dc40789cfdfa4eb72a5bf5cf227d3781
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-24 17:16:28 +0000

    Update git server version and install more helm charts

--- modules/gw/adaptation/main.tf
+++ modules/gw/adaptation/main.tf
@@ -5,6 +5,34 @@ resource "rancher2_project" "icap-service" {
   wait_for_cluster = true
 }
 
+data "rancher2_project" "system" {
+  provider         = rancher2.adm
+  cluster_id = var.cluster_ids[0]
+  name = "System"
+}
+
+resource "rancher2_multi_cluster_app" "systemclusterrole" {
+  provider    = rancher2.adm
+  catalog_name = var.catalogue_name
+  name         = "systemclusterrole"
+  targets {
+    project_id = data.rancher2_project.system.id
+  }
+  template_name = "systemclusterrole"
+  roles         = ["cluster-owner"]
+}
+
+resource "rancher2_multi_cluster_app" "rabbitmq" {
+  provider    = rancher2.adm
+  catalog_name = var.catalogue_name
+  name         = "rabbitmq"
+  targets {
+    project_id = rancher2_project.icap-service.id
+  }
+  template_name = "icap-rabbitmq"
+  roles         = ["project-owner"]
+}
+
 resource "rancher2_multi_cluster_app" "icapadaptation" {
   provider     = rancher2.adm
   catalog_name = var.catalogue_name
@@ -13,30 +41,5 @@ resource "rancher2_multi_cluster_app" "icapadaptation" {
     project_id = rancher2_project.icap-service.id
   }
   template_name = "icap-adaptation"
-  roles         = ["cluster-owner"]
+  roles         = ["project-owner"]
 }
-//
-//resource "rancher2_multi_cluster_app" "rabbitmq" {
-//  provider    = rancher2.admin
-//  catalog_name = var.catalogue_name
-//  name         = "rabbitmq"
-//  dynamic "targets" {
-//    for_each = var.cluster_ids
-//    content {
-//      project_id = targets.value
-//    }
-//  }
-//  template_name = "icap-rabbitmq"
-//  roles         = ["cluster-owner"]
-//}
-//
-//resource "rancher2_multi_cluster_app" "systemclusterrole" {
-//  provider    = rancher2.admin
-//  catalog_name = var.catalogue_name
-//  name         = "systemclusterrole"
-//  targets {
-//    project_id = var.cluster_ids[0]
-//  }
-//  template_name = "systemclusterrole"
-//  roles         = ["cluster-owner"]
-//}
