e842f55cc71483e2bffcc86218618937246948d9
Tass Skoudros <tass@servana.net>
2020-11-26 16:57:02 +0000

    fix the cluster module

--- workspace/proto-multi-clusters/main.tf
+++ workspace/proto-multi-clusters/main.tf
@@ -103,7 +103,26 @@ data "azurerm_key_vault_secret" "az-subscription-id" {
 module "icap_clusters" {
   source                       = "../../modules/gw/cluster"
   for_each                     = local.azure_icap_clusters
-  cluster_quantity             = var.cluster_quantity
+
+  cluster_quantity             = each.value.cluster_quantity
+  suffix                       = each.value.suffix
+  azure_region                 = each.value.azure_region
+  rancher_projects             = each.value.rancher_projects
+  cluster_backend_port         = each.value.backend_port
+  cluster_public_port          = each.value.public_port
+  cluster_address_space        = each.value.cluster_address_space
+  cluster_subnet_cidr          = each.value.cluster_subnet_cidr
+  cluster_subnet_prefix        = each.value.cluster_subnet_prefix
+  os_publisher                 = each.value.os_publisher
+  os_offer                     = each.value.os_offer
+  os_sku                       = each.value.os_sku
+  os_version                   = each.value.os_version
+  master_scaleset_size         = each.value.master_scaleset_size
+  master_scaleset_admin_user   = each.value.master_scaleset_admin_user
+  master_scaleset_sku_capacity = each.value.master_scaleset_sku_capacity
+  worker_scaleset_size         = each.value.master_scaleset_size
+  worker_scaleset_admin_user   = each.value.master_scaleset_admin_user
+  worker_scaleset_sku_capacity = each.value.master_scaleset_sku_capacity
   organisation                 = var.organisation
   environment                  = var.environment
   rancher_admin_url            = local.rancher_api_url
@@ -112,31 +131,14 @@ module "icap_clusters" {
   rancher_network              = local.rancher_network
   rancher_resource_group       = local.rancher_resource_group
   service_name                 = local.service_name
-  suffix                       = var.suffix
-  azure_region                 = var.azure_region
   client_id                    = data.azurerm_key_vault_secret.az-client-id.value
   client_secret                = data.azurerm_key_vault_secret.az-client-secret.value
   subscription_id              = data.azurerm_key_vault_secret.az-subscription-id.value
   tenant_id                    = var.tenant_id
-  cluster_backend_port         = var.backend_port
-  cluster_public_port          = var.public_port
-  cluster_address_space        = var.cluster_address_space
-  cluster_subnet_cidr          = var.cluster_subnet_cidr
-  cluster_subnet_prefix        = var.cluster_subnet_prefix
   public_key_openssh           = local.public_key_openssh
   rancher_network_id           = local.rancher_network_id
-  os_publisher                 = var.os_publisher
-  os_offer                     = var.os_offer
-  os_sku                       = var.os_sku
-  os_version                   = var.os_version
-  rancher_projects             = var.rancher_projects
-  master_scaleset_size         = var.master_scaleset_size
-  master_scaleset_admin_user   = var.master_scaleset_admin_user
-  master_scaleset_sku_capacity = var.master_scaleset_sku_capacity
-  worker_scaleset_size         = var.master_scaleset_size
-  worker_scaleset_admin_user   = var.master_scaleset_admin_user
-  worker_scaleset_sku_capacity = var.master_scaleset_sku_capacity
 }
+
 /*
 module "admin_cluster" {
   source                       = "../../modules/gw/standalone-cluster"
