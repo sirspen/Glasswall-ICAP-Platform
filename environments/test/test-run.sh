a3aaea5e713b75a2689afabb74968d68b215a11b
Tass Skoudros <tass@servana.net>
2020-12-11 16:44:19 +0000

    a way to test the icap service

---
+++ environments/test/test-run.sh
@@ -0,0 +1,3 @@
+thepath="$pwd"
+docker run -ti --entrypoint /bin/bash -v $thepath:/opt glasswallsolutions/c-icap-client:manual-v1
+#/usr/local/c-icap/bin/c-icap-client -i gw-icap-dev-nor.icap-proxy.curlywurly.me:443 -s gw_rebuild -f /opt/sample.pdf -o /opt/sample-rebuild.pdf -v
