c76897de61f1b6b50dea2e3308c0ad07baca349b
Tass Skoudros <tass@servana.net>
2020-12-11 16:47:34 +0000

    adding tests for both clusters

--- environments/test/test-run.sh
+++ environments/test/test-run.sh
@@ -1,3 +1,5 @@
 thepath="$pwd"
 docker run -ti --entrypoint /bin/bash -v $thepath:/opt glasswallsolutions/c-icap-client:manual-v1
-/usr/local/c-icap/bin/c-icap-client -i gw-icap-dev-nor.icap-proxy.curlywurly.me:443 -s gw_rebuild -f /opt/sample.pdf -o /opt/sample-rebuild.pdf -v
+/usr/local/c-icap/bin/c-icap-client -i gw-icap-dev-nor.icap-proxy.curlywurly.me:443 -s gw_rebuild -f /opt/sample.pdf -o /opt/sample-rebuild-nor.pdf -v
+/usr/local/c-icap/bin/c-icap-client -i gw-icap-dev-ukw.icap-proxy.curlywurly.me:443 -s gw_rebuild -f /opt/sample.pdf -o /opt/sample-rebuild-ukw.pdf -v
+
