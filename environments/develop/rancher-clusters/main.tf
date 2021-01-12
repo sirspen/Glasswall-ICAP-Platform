8f43cd0e79259ee860db31e246785c6f310d0b5b
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-01-12 12:17:07 +0000

    Fix merge conflict

--- environments/develop/rancher-clusters/main.tf
+++ environments/develop/rancher-clusters/main.tf
@@ -13,16 +13,8 @@ module "rancher_clusters" {
     environment         = "dev"
     branch              = "develop"
     #cluster quantity is 1 per region
-<<<<<<< HEAD
     icap_cluster_quantity  = 1
     icap_master_scaleset_sku_capacity = 1
     icap_worker_scaleset_sku_capacity = 1
     dns_zone = "dev.icap-proxy.curlywurly.me"
 }
-=======
-    icap_cluster_quantity                      = 1
-    icap_master_scaleset_sku_capacity          = 1
-    icap_worker_scaleset_sku_capacity          = 1
-}
-
->>>>>>> 736133b7ee49f6db295034ef6b507d7b6540916a
