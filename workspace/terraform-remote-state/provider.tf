ddf9dc3c074da43eb4bf368c55fda22151d24475
Tass Skoudros <tass@servana.net>
2021-01-13 10:02:28 +0000

    removed some unused defaults

--- workspace/terraform-remote-state/provider.tf
+++ workspace/terraform-remote-state/provider.tf
@@ -1,6 +1,5 @@
 
 provider "azurerm" {
   version   = "=2.30.0"
-  tenant_id = "7049e6a3-141d-463a-836b-1ba40d3ff653"
   features {}
 }
--- workspace/terraform-remote-state/variables.tf
+++ workspace/terraform-remote-state/variables.tf
@@ -1,7 +1,6 @@
 variable "organisation" {
   description = "Metadata Organisation"
   type        = string
-  default     = "gw"
 }
 
 variable "environment" {
@@ -23,19 +22,16 @@ variable "suffix" {
 variable "azure_region" {
   description = "Metadata Azure Region"
   type        = string
-  default     = "ukwest"
 }
 
 variable "subscription_id" {
   description = "Subscription ID"
   type        = string
-  default     = "b8177f86-515f-4bff-bd08-1b9535dbc31b"
 }
 
 variable "tenant_id" {
   description = "Tenant ID"
   type        = string
-  default     = "7049e6a3-141d-463a-836b-1ba40d3ff653"
 }
 
 variable "storage_tier" {
