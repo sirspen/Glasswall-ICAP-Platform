e458a9332f79c9e07669f32e5f6886bc1ed7aad6
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-01-13 15:30:10 +0000

    Add azure region

--- environments/develop/rancher-bootstrap/main.tf
+++ environments/develop/rancher-bootstrap/main.tf
@@ -15,5 +15,8 @@ module "rancher_server" {
   project                = "rancher-server"
   suffix                 = "z1"
   git_server_version     = "1.87"
+  azure_region           = "ukwest" # ukwest
   dns_zone_name          = "dev.icap-proxy.curlywurly.me"
+  subscription_id        = "b8177f86-515f-4bff-bd08-1b9535dbc31b"
+  tenant_id              = "7049e6a3-141d-463a-836b-1ba40d3ff653"
 }
